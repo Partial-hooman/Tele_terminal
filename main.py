ef14c156fbe990d9b8588f9fd69110b95fbc4878
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2022-09-13 20:45:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,3 +22,27 @@ def terminate(process):
     process.stdin.close()
     process.terminate()
     process.wait(timeout=0.2)
+
+    
+    
+    
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
+  
+updater = Updater("your_own_API_Token got from BotFather",
+                  use_context=True)
+  
+  
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Hello sir, Welcome to the Bot.Please write\
+        /help to see the commands available.")
+  
+def help(update: Update, context: CallbackContext):
+    update.message.reply_text("""Available Commands :-
+    i) send the command as normal message to execute
+    ii) To enter inputs, use ':' as prefix before the input, ex- ':start_server' """)
